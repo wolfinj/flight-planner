761d073206b3f06883640bb02722e277b284505b
wolfinj <wolfinj@gmail.com>
2022-10-03 19:10:57 +0300

    Add migration files

---
+++ FlightPlanner/Migrations/20221002183941_Initial.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlightPlanner.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Airports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Country = table.Column<string>(type: "TEXT", nullable: false),
+                    City = table.Column<string>(type: "TEXT", nullable: false),
+                    AirportCode = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Airports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FromId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ToId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Carrier = table.Column<string>(type: "TEXT", nullable: false),
+                    DepartureTime = table.Column<string>(type: "TEXT", nullable: false),
+                    ArrivalTime = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Flights_Airports_FromId",
+                        column: x => x.FromId,
+                        principalTable: "Airports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Flights_Airports_ToId",
+                        column: x => x.ToId,
+                        principalTable: "Airports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_FromId",
+                table: "Flights",
+                column: "FromId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_ToId",
+                table: "Flights",
+                column: "ToId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flights");
+
+            migrationBuilder.DropTable(
+                name: "Airports");
+        }
+    }
+}
